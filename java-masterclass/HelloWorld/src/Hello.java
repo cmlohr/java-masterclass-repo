3194cc8e36a47d74a97cc0a2d75e3e2f906c0524
Colleen Lohr <cmlohr@outlook.com>
2021-10-25 12:04:38 -0500

    applying math within int declaration

--- java-masterclass/HelloWorld/src/Hello.java
+++ java-masterclass/HelloWorld/src/Hello.java
@@ -1,10 +1,13 @@
 public class Hello {
 
     public static void main(String[] args) {
-    System.out.println("Hello, Buddy");
+        System.out.println("Hello, Buddy");
 
     int Num = 3;
-    System.out.println(Num);
+        System.out.println(Num);
+
+    int newNum = (10 + 5) + (2 * 10);
+        System.out.println(newNum);
     }
 }
 

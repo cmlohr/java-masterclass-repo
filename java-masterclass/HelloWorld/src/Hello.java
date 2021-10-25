daef0b8245b63d27f947a58eb546c6f9e1b6e8b7
Colleen Lohr <cmlohr@outlook.com>
2021-10-25 12:14:29 -0500

    Adding more expressions

--- java-masterclass/HelloWorld/src/Hello.java
+++ java-masterclass/HelloWorld/src/Hello.java
@@ -8,6 +8,12 @@ public class Hello {
 
     int newNum = (10 + 5) + (2 * 10);
         System.out.println(newNum);
+
+    int thisIsTwelve = (10-56) + (23 * -45) - (45 / 6) / (2 * 6) + (10 * 100) - (10 * -10) + (-7);
+        System.out.println(thisIsTwelve);
+
+    int thisIsSix = (36 / 6);
+        System.out.println(thisIsSix);
     }
 }
 

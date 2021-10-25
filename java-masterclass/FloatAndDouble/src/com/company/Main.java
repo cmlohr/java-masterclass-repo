848ac668f41428b6feeebddfaab18218041ff5fe
Colleen Lohr <cmlohr@outlook.com>
2021-10-25 13:35:41 -0500

    Added examples to FloatAndDouble

--- java-masterclass/FloatAndDouble/src/com/company/Main.java
+++ java-masterclass/FloatAndDouble/src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // Single & Double Precision
+        //Single precision = 32 bits (width)
+        //Double precision = 64 bits (width)
+
+        float myMinFloat = Float.MIN_VALUE;
+        float myMaxFloat = Float.MAX_VALUE;
+
+        System.out.println("Min Float = " + myMinFloat);
+        System.out.println("Max Float = " + myMaxFloat);
+
+
+
     }
 }

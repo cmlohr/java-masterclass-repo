f22926c9ed21804f86913893b8c4fe9b94e40cd4
Colleen Lohr <cmlohr@outlook.com>
2021-10-25 19:28:13 -0500

    Added Operators Notes

---
+++ java-masterclass/Operators/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int result = 1 + 2; // 1 + 2 = 3
+        System.out.println(result);
+
+        int previousResult = result;
+        System.out.println("previousResult = " + previousResult);
+
+        result = result - 1; // 3 - 1 = 2
+        System.out.println("3 - 1 = " + result);
+        System.out.println("previousResult = " + previousResult); //previousResult doesn't get updated
+
+        result = result * 10; // 2 * 10
+        System.out.println("2 * 10 = " + result);
+
+        result = result / 5; // 20 / 5
+        System.out.println("20 / 5 = " + result);
+        
+        result = result % 3; // the remainder of (4 % 3) = 1
+        System.out.println("4 % 3 = " + result);
+    }
+}

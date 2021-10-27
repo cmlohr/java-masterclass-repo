1f8d0ecd638142266f2d2f2156a357650659b81b
Colleen Lohr <cmlohr@outlook.com>
2021-10-27 17:39:03 -0500

    IfKeyword example added

--- java-masterclass/CodeBlocks&IfKeywords/src/com/company/Main.java
+++ java-masterclass/CodeBlocks&IfKeywords/src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 public class Main {
 
+    //Code Block and If Keyword examples
     public static void main(String[] args) {
 	boolean gameOver = true;
     int score = 10000;
@@ -14,13 +15,14 @@ public class Main {
     if (score < 10000 && score >= 1000) {
         int lostStamina = staminaBar - 40;
         int lostHappy = happyBar - 100;
-        System.out.println("You did not pass on to the next round, try again?" + "\nStamina: " + lostStamina + "\nHappy: " + lostHappy);
+        System.out.println("You did not pass on to the next round, try again?" + "\nStamina: -" + lostStamina +
+                            "\nHappy: -" + lostHappy);
     } else if (score < 1000) {
         int death = healthBar - 75;
-        System.out.println("Death" + "\nHealth: " + death);
+        System.out.println("Death" + "\nHealth: --" + death);
     } else {
         int newLevel = lastLevel + 1;
         int newKarma = karma + 5;
-        System.out.println("Congrats, onto level " + newLevel + "!" + "\nKarma: " + newKarma);
+        System.out.println("Congrats, onto level " + newLevel + "!" + "\nKarma: +" + newKarma);
     }
 }}
